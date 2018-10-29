cf8d853de51d2d7be49713e3c0e2a792f39e4e3e
Vibhor Mittal <43182527+vibhormittal214@users.noreply.github.com>
2018-10-29 21:13:38 +0530

    Add files via upload
    An efficient program to print prime factors of a number

---
+++ prime factore.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+   int i,n;
+   cin>>n;
+   while(n%2==0){
+    cout<<"2 ";
+    n=n/2;
+   }
+   for(i=3;i<=sqrt(n);i=i+2){
+    while(n%i==0){
+        cout<<i<<" ";
+        n=n/i;
+    }
+   }
+   if(n>2){
+    cout<<n;
+   }
+   return 0;
+
+}
